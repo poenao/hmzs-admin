ab6c08b3783e5888307c402ff46d56095cb247a0
poenao <940837938@qq.com>
2026-04-02 04:31:05 +0800

    按钮权限-注册自定义指令通过指令里面写逻辑绑定在按钮上

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import ElementPlus from "element-plus";
-import zhCn from "element-plus/es/locale/lang/zh-cn";
-import "element-plus/dist/index.css";
-import "./styles/index.scss";
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
+import './styles/index.scss'
 
-import { createPinia } from "pinia";
-import router from "./router";
+import { createPinia } from 'pinia'
+import router from './router'
 // 注意：在router引入之后引入
-import "./permission";
-
-const app = createApp(App);
+import './permission'
+import { auth } from '../src/directive/index'
+const app = createApp(App)
 
 app.use(ElementPlus, {
-  locale: zhCn,
-});
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+  locale: zhCn
+})
+app.use(createPinia())
+app.use(router)
+app.directive('auth-btn', auth)
+// 全局注册组件
+// Removed app.component registration for 'auth' as it is a directive
+app.mount('#app')
